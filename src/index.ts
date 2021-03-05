119876fee1a0d8882afd60f935b98127cb2f5e45
Vili <vili@consolidity.com>
2021-03-05 18:47:01 +0100

    set background from TS file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-const cardElement = document.querySelector('.card');
+const cardElement: HTMLDivElement = document.querySelector('.card');
 
 for (let index: number = 0; index < 15; index++) {
-  const clone = cardElement.cloneNode();
+  const clone: Node = cardElement.cloneNode(true);
   document.getElementById('game-area').appendChild(clone);
+}
+
+const cards: NodeListOf<HTMLDivElement> = document.querySelectorAll('.card');
+
+for (const card of cards) {
+  card.style.backgroundImage = 'url(../dist/assets/1.jpg)';
 }
