96dcdea891b4889d661d29f40323fd8d0760a945
Vili <vili@consolidity.com>
2021-03-12 19:07:07 +0100

    hot reloaded full-stack app

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,4 +18,22 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    index: 'index.html',
+    port: 3000,
+    disableHostCheck: true,
+    compress: true,
+    contentBase: path.join(__dirname, 'dist'),
+    hot: true,
+    historyApiFallback: true,
+    compress: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        secure: false,
+      },
+    },
+  },
 };
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const app = express();
 const port = 5000;
 
-app.use(express.static('client/dist'));
+app.use(express.static('client/dist', { index: false }));
+
+app.get('*', (_, res) => res.sendFile(path.resolve('client/dist', 'index.html')));
 
 app.listen(port, () => console.log(`app is listening on port: ${port}`));
